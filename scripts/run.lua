eef3425c50f84ab386f9df674cb467639b8bd55b
Mathias Hauan Arbo <mathiasarbo@gmail.com>
2019-05-10 16:00:14 +0200

    Added info on how to set IP

--- scripts/run.lua
+++ scripts/run.lua
@@ -12,8 +12,14 @@ depl = tc:getPeer("Deployer")
 -- Interface with ROS:
 depl:import("rtt_ros")
 
--- Load my component
+-- Load RSI component
 depl:import("rtt_rsi")
 depl:loadComponent("rsi", "rtt_rsi::RSIComponent")
 rsi = depl:getPeer("rsi")
+
+-- Set ip address of robot
+robot_ip = rsi:getProperty("local_host")
+robot_port = rsi:getProperty("local_port")
+robot_ip:set("127.0.0.1") -- change this to the IP to connect
+robot_port:set(49152) -- Listening port
 rsi:configure()
